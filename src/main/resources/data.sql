9ba535f8336ec786e38fe63687e47e7e5656b28e
nurgazy-07 <temiralievnurgazy07@gmail.com>
2024-04-10 04:17:41 +0600

    connection with swagger and added data initialization!

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into users
+VALUES (1, 'address', 'admin@gmail.com', 'administrator', 'https://s3.amazonaws.com/yourbucket/users/admin.png',
+        'admin', '$2a$12$2MAKJ64YWOW4FCA0rO4xOuULF6QbxFWl1AnTpfQViwtYQKB9H.kiG', '+996303333333', 'ADMIN'),
+       (2, 'luksemburgskay23', 'nur@gmail.com', 'nurgazy','https://s3.amazonaws.com/yourbucket/users/nurgazy.png',
+        'temiraliev', '$2a$12$MKHTlq.lDW5TtEkEDn/mk.h3y25/0qCKIaPPKAPOlFmxn1dMlxCj2', '+996302233333', 'USER');
+
+insert into contacts (id, firstname, lastname, email, phone_number, message)
+values (1, 'Nurislam', 'Toygonbaev', 'nurislam@gmail.com', '+996701111111', 'hello'),
+       (2, 'Zhigit', 'Turumbekov', 'zhigit@gmail.com', '+996702222222', 'i have things to do!'),
+       (3, 'Aiturgan', 'Maksatbek kyzy', 'aiturga@gmail.com', '+996703333333', 'What''s happened?');
