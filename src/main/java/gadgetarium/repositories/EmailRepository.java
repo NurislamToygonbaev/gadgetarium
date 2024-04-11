edbe7942925c9d99b734ffeca477e25801b05da4
nurgazy-07 <temiralievnurgazy07@gmail.com>
2024-04-12 05:13:28 +0600

    as

--- src/main/java/gadgetarium/repositories/EmailRepository.java
+++ src/main/java/gadgetarium/repositories/EmailRepository.java
@@ -1,8 +1,4 @@
 package gadgetarium.repositories;
-<<<<<<< HEAD
-=======
-
->>>>>>> aa44a40e05cfc5976e41207521cb0a5373355237
 import gadgetarium.entities.EmailAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
