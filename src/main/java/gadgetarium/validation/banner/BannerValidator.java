81e8199e9193bd1fc4006c4b9a625f3e4cd90b03
Aiturganm <aiturganmaksatbekkyzy04@gmail.com>
2024-04-12 20:50:06 +0600

    Banner

---
+++ src/main/java/gadgetarium/validation/banner/BannerValidator.java
@@ -0,0 +1,13 @@
+package gadgetarium.validation.banner;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class BannerValidator implements ConstraintValidator<BannerValidation, List<String>> {
+
+    @Override
+    public boolean isValid(List<String> banners, ConstraintValidatorContext constraintValidatorContext) {
+        return banners.size() <= 6;
+    }
+}
