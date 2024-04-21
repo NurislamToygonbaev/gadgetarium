396fb023f9866b3d4d62f32229b26a29c4a64883
Aiturganm <aiturganmaksatbekkyzy04@gmail.com>
2024-04-21 20:02:04 +0600

    Send a new letter to user's email

--- src/main/java/gadgetarium/services/MailingService.java
+++ src/main/java/gadgetarium/services/MailingService.java
@@ -1,4 +1,9 @@
 package gadgetarium.services;
 
+import gadgetarium.dto.request.NewsLetterRequest;
+import gadgetarium.dto.response.NewsLetterResponse;
+
 public interface MailingService {
+
+    NewsLetterResponse sendNewsLetter(NewsLetterRequest newsLetterRequest);
 }
