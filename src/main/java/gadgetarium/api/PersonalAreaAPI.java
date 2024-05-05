665de4d2b366647f035ae98d7407b9d80067ce7c
nurgazy-07 <temiralievnurgazy07@gmail.com>
2024-05-05 18:57:45 +0600

    add api historyOfOrders

---
+++ src/main/java/gadgetarium/api/PersonalAreaAPI.java
@@ -0,0 +1,12 @@
+package gadgetarium.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/personal-area")
+public class PersonalAreaAPI {
+
+}
