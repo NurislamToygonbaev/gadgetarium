cf785a89f4c4c2ac5a6628a6411df4ff2787bc6f
nurgazy-07 <temiralievnurgazy07@gmail.com>
2024-05-13 01:07:31 +0600

    api history or orders and profile

--- src/main/java/gadgetarium/api/PersonalAreaAPI.java
+++ src/main/java/gadgetarium/api/PersonalAreaAPI.java
@@ -5,8 +5,10 @@ import gadgetarium.dto.request.CurrentUserProfileRequest;
 import gadgetarium.dto.request.UserImageRequest;
 import gadgetarium.dto.response.*;
 import gadgetarium.services.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,26 +20,36 @@ public class PersonalAreaAPI {
 
     private final OrderService orderService;
 
+    @PreAuthorize("hasAnyAuthority({'USER'})")
+    @Operation(summary = "Посмотреть весь истории заказов.", description = "авторизация: ПОЛЬЗОВАТЕЛЬ")
     @GetMapping("/view-all-history")
     public List<AllOrderHistoryResponse> getAllOrdersHistory(){
         return orderService.getAllOrdersHistory();
     }
 
+    @PreAuthorize("hasAnyAuthority({'USER'})")
+    @Operation(summary = "Посмотреть один историю заказа.", description = "авторизация: ПОЛЬЗОВАТЕЛЬ")
     @GetMapping("/view-order/{orderId}")
     public OrderHistoryResponse getOrderHistoryById(@PathVariable Long orderId){
         return orderService.getOrderHistoryById(orderId);
     }
 
+    @PreAuthorize("hasAnyAuthority({'USER'})")
+    @Operation(summary = "Изменить профиль.", description = "авторизация: ПОЛЬЗОВАТЕЛЬ")
     @PostMapping("/edit-profile")
     public CurrentUserProfileResponse editProfile(@RequestBody @Valid CurrentUserProfileRequest currentUserProfileRequest){
         return orderService.editProfile(currentUserProfileRequest);
     }
 
+    @PreAuthorize("hasAnyAuthority({'USER'})")
+    @Operation(summary = "Изменить фото профиля.", description = "авторизация: ПОЛЬЗОВАТЕЛЬ")
     @PutMapping("/add-image")
     public UserImageResponse addPhotoAndEdit(@RequestBody @Valid UserImageRequest userImageRequest ){
         return orderService.addPhotoAndEdit(userImageRequest);
     }
 
+    @PreAuthorize("hasAnyAuthority({'USER'})")
+    @Operation(summary = "Сменить пароль.", description = "авторизация: ПОЛЬЗОВАТЕЛЬ")
     @PostMapping("/change-password")
     public HttpResponse changePassword(@RequestBody @Valid ChangePasswordRequest changePasswordRequest){
         return orderService.changePassword(changePasswordRequest);
