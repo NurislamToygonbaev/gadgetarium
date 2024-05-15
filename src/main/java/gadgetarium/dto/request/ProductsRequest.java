52931e31775f1a9c555fed57bdb78bdb3cc29fbb
Zhigit Turumbekov <zhigitturumbekov@gmail.com>
2024-05-15 09:50:43 +0600

    Adding validation for the number of SIM cards.

--- src/main/java/gadgetarium/dto/request/ProductsRequest.java
+++ src/main/java/gadgetarium/dto/request/ProductsRequest.java
@@ -2,6 +2,7 @@ package gadgetarium.dto.request;
 
 import gadgetarium.enums.Memory;
 import gadgetarium.enums.Ram;
+import gadgetarium.validation.countSim.CountSimValidation;
 import lombok.Builder;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public record ProductsRequest(
         String mainColour,
         Memory memory,
         Ram ram,
+        @CountSimValidation
         int countSim,
         List<String> images
 ) {
