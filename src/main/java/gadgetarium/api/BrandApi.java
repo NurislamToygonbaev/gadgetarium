72cb585542fb406b3032c9c3f101800158118020
mukhammed.codes <muhammedtoichubai@gmail.com>
2024-05-23 11:46:20 +0600

    add media type

--- src/main/java/gadgetarium/api/BrandApi.java
+++ src/main/java/gadgetarium/api/BrandApi.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.MediaType;
 
 import java.util.List;
 
@@ -23,7 +24,8 @@ public class BrandApi {
 
     @PreAuthorize("hasAuthority('ADMIN')")
     @Operation(summary = "Сохранение бренда", description = "Авторизация: АДМИНСТРАТОР")
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+                 produces = MediaType.APPLICATION_JSON_VALUE)
     public HttpResponse saveBrand(@RequestParam(value = "file") MultipartFile file,
                                   @RequestParam String brandName) {
         return brandService.saveBrand(file, brandName);
