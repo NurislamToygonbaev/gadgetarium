4f136c6b744036c017d7b334f5445fe708f051a5
Nurislam Toygonbaev <ntoygonbaev098@gmail.com>
2024-06-05 14:53:40 +0600

    banner get all and find by id

--- src/main/java/gadgetarium/repositories/BannerRepository.java
+++ src/main/java/gadgetarium/repositories/BannerRepository.java
@@ -1,6 +1,16 @@
 package gadgetarium.repositories;
+import gadgetarium.dto.response.GetAllBannerResponse;
 import gadgetarium.entities.Banner;
+import gadgetarium.exceptions.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BannerRepository extends JpaRepository<Banner, Long> {
+
+    default Banner getBannerById(Long bannerId){
+        return findById(bannerId).orElseThrow(() ->
+                new NotFoundException("Banner with id: "+bannerId+" not found"));
+    }
 }
