1e5bbeae275a91b98f8845fd516e03b6245c893c
nurgazy-07 <temiralievnurgazy07@gmail.com>
2024-06-12 17:58:54 +0600

    refactor LocalDate time exeption

--- src/main/java/gadgetarium/configs/paypal/PayPalConfig.java
+++ src/main/java/gadgetarium/configs/paypal/PayPalConfig.java
@@ -1,6 +1,7 @@
 package gadgetarium.configs.paypal;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +20,9 @@ public class PayPalConfig {
 
     @Bean
     public ObjectMapper objectMapper(){
-        return new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
     }
 
     @Bean
